178e0c4351ccac190c7d22b5d6b0b0c5a13a7d00
Tyler Tierney <tytierney@yahoo.com>
2022-05-08 16:27:58 -0400

    Still working on roomCode integration

--- index.js
+++ index.js
@@ -19,42 +19,52 @@ let rooms = [];
 let players = [];
 let turns = [];
 
-const getRoom = (rooms, roomCode) => {
-  return rooms.filter((room) => room.roomCode === roomCode)[0];
+const getRoomIndex = (roomCode) => {
+  return rooms.findIndex((room) => room.roomCode === roomCode);
 };
 
 io.on("connection", (socket) => {
   socket.on("draw", (drawingData, roomCode) => {
-    turns[turns.length - 1].drawing = drawingData;
-    io.to(roomCode).emit("draw", drawingData, turns);
+    const roomIndex = getRoomIndex(roomCode);
+    const turnsIndex = rooms[roomIndex].turns.length - 1;
+    rooms[roomIndex].turns[turnsIndex].drawing = drawingData;
+    io.to(roomCode).emit("draw", drawingData, rooms[roomIndex].turns);
   });
 
   socket.on("createLobby", (playerObj, roomCode) => {
-    players.push(playerObj);
+    const roomObj = { roomCode: roomCode, players: [playerObj], turns: [] };
+    rooms.push(roomObj);
+    console.log(rooms);
     socket.join(roomCode);
-    io.to(roomCode).emit("createLobby", players);
+    io.to(roomCode).emit("createLobby", roomObj.players);
   });
 
   socket.on("joinLobby", (playerObj, roomCode) => {
-    players.push(playerObj);
+    const roomIndex = getRoomIndex(roomCode);
+    rooms[roomIndex].players.push(playerObj);
     socket.join(roomCode);
-    io.to(roomCode).emit("joinLobby", players, roomCode);
+    io.to(roomCode).emit("joinLobby", rooms[roomIndex].players, roomCode);
   });
 
   socket.on("selectWord", (word, roomCode) => {
     let time = 90;
-    turns[turns.length - 1].word = word;
     setInterval(() => {
       if (time > -1) {
         io.to(roomCode).emit("setTimer", time);
       }
       time--;
     }, 1000);
+    const roomIndex = getRoomIndex(roomCode);
+    const turns = [...rooms[roomIndex].turns];
+    turns[turns.length - 1].word = word;
+    rooms[roomIndex].turns = turns;
     io.to(roomCode).emit("selectWord", turns);
   });
 
   socket.on("startGame", (roomCode) => {
-    const firstTurnPlayer = players[Math.floor(Math.random() * players.length)];
+    const roomIndex = getRoomIndex(roomCode);
+    const firstTurnPlayer =
+      rooms[roomIndex].players[Math.floor(Math.random() * players.length)];
     const turnObj = {
       word: "banana",
       drawing: "",
@@ -72,19 +82,27 @@ io.on("connection", (socket) => {
       words.splice(index, 1);
     }
 
-    turns.push(turnObj);
-    io.to(roomCode).emit("startGame", turns);
+    rooms[roomIndex].turns.push(turnObj);
+    io.to(roomCode).emit("startGame", [turnObj]);
   });
 
   socket.on("guess", (guess, roomCode) => {
-    turns[turns.length - 1].guesses.push(guess);
+    const roomIndex = getRoomIndex(roomCode);
+    const turns = [...rooms[roomIndex].turns];
+    rooms[roomIndex].turns[turns.length - 1].guesses.push(guess);
     io.to(roomCode).emit("guess", guess, turns);
   });
 
+  socket.on("getCurrentGame", (roomCode) => {
+    const roomIndex = getRoomIndex(roomCode);
+    socket.join(roomCode);
+    io.to(roomCode).emit("getCurrentGame", rooms[roomIndex]);
+  });
+
   socket.on("endGame", (roomCode) => {
-    console.log(roomCode);
-    turns = [];
-    players = [];
+    const roomIndex = getRoomIndex(roomCode);
+    rooms[roomIndex].turns = [];
+    rooms[roomIndex].players = [];
     io.to(roomCode).emit("endGame");
   });
 });
